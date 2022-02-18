a00785e55f12442ae66dff1502e226a53add19fd
pscesar <cesar.de.vila.al@iespoligonosur.org>
2022-02-18 14:21:28 +0100

    Clase IAW 18/02

---
+++ formulario.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+	Nombre: <?php echo $_POST["nombre"]; ?><br>
+	Edad: <?php echo $_POST["edad"]; ?><br>
+	Email: <?php echo $_POST["email"]; ?>
+</body>
+</html>
---
+++ practica_4.3.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="formulario.php" method="post">
+    Nombre: <br><input type="text" name="nombre"><br><br>
+    Edad: <br><input type="text" name="edad"><br><br>
+    Email: <br><input type="text" name="email"><br>
+    <br><input type="submit" value="Enviar">
+</form>
+</body>
+</html>
